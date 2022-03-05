9c72c467fc81a210c6b381323df6ea04f5b8c88d
DuCanhGH <75556609+DuCanhGH@users.noreply.github.com>
2022-03-05 21:10:44 +0700

    v1.0.1

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.get("*", (req, res) => {
     `);
 });
 
-const port = 3000;
+const port = process.env.PORT | 3000;
 
 app.listen(port, () => {
     console.log(`🚀 Server started on port ${port}`);
