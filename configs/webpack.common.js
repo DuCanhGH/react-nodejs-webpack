db8228d9e0210fbbc676c0e13329bdeaca327a6c
DuCanhGH <75556609+DuCanhGH@users.noreply.github.com>
2022-03-06 16:34:15 +0700

    v1.1.5

--- configs/webpack.common.js
+++ configs/webpack.common.js
@@ -45,9 +45,7 @@ const common = {
     },
     plugins: [new MiniCssExtractPlugin(), new webpack.WatchIgnorePlugin({ paths: [appAssetsManifest] }), new webpack.DefinePlugin({
         'process.env.ASSETS_MANIFEST': JSON.stringify(appAssetsManifest),
-        'process.env': {
-            PUBLIC_DIR: JSON.stringify(path.resolve(rootDir, process.env.NODE_ENV !== "development" ? "build/public" : "public")),
-        }
+        'process.env.PUBLIC_DIR': JSON.stringify(path.resolve(rootDir, process.env.NODE_ENV !== "development" ? "build/public" : "public")),
     })],
 };
 
