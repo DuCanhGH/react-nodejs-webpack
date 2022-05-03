586f78e2f08f486851c9a75753e7605b4baede16
DuCanhGH <75556609+DuCanhGH@users.noreply.github.com>
2022-05-03 12:46:32 +0700

    Update client.tsx

--- src/client.tsx
+++ src/client.tsx
@@ -2,4 +2,8 @@ import { hydrateRoot } from 'react-dom/client';
 import Dak2 from 'App';
 import { BrowserRouter } from "react-router-dom";
 
-hydrateRoot(document.getElementById('root')!, <BrowserRouter><Dak2 /></BrowserRouter>);
+const container = document.getElementById("root"); //HTML template must have something that uses this id: root.
+
+if (!container) throw new Error('Failed to find the root element');
+
+hydrateRoot(container, <BrowserRouter><Dak2 /></BrowserRouter>);
